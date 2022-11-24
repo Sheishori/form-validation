6ef6d71e1c2e3304ee24d0289b9d8a47c1c5d80c
Sheishori <sheishori@gmail.com>
2022-11-24 15:17:38 +0100

    Reset field validity when typing in

--- scripts.js
+++ scripts.js
@@ -4,3 +4,14 @@ const country = document.getElementById('country');
 const zipCode = document.getElementById('zip-code');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirm-password');
+
+function resetValidity() {
+	this.setCustomValidity("");
+	this.checkValidity();
+};
+
+email.addEventListener('input', resetValidity);
+country.addEventListener('input', resetValidity);
+zipCode.addEventListener('input', resetValidity);
+password.addEventListener('input', resetValidity);
+confirmPassword.addEventListener('input', resetValidity);
