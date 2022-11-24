c824d62839f544154afd5f15909a6dcb1104e120
Sheishori <sheishori@gmail.com>
2022-11-24 14:36:33 +0100

    Add query selectors

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('form');
+const email = document.getElementById('email');
+const country = document.getElementById('country');
+const zipCode = document.getElementById('zip-code');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm-password');
